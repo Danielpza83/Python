d8dcb835be3123658f6ab44684cb9194d957fbd2
DanielMaestre <dmaestre1490@gmail.com>
2023-07-05 19:26:38 -0500

    Mi primer commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="My Package",
+    version="1.0",
+    description="Package of variable and matematics functions",
+    author="Daniel Maestre",
+    author_email="dmaestre1490@gmail.com",
+    packages=["package"]
+)
